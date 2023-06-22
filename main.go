f465b794535851eb81f36e6b6fb764bb1fca1efe
Iskandar Isman <iskandareizyh12@gmail.com>
2023-06-22 13:22:36 +0800

    wip: add io package

--- main.go
+++ main.go
@@ -4,24 +4,41 @@ import (
 	"fmt"
 	"log"
 	"net/http"
-
+	
 	"github.com/gorilla/mux"
+	"io/ioutil"
 )
 
+// Make a dummy database
 type event struct {
 	ID string `json:"ID"`
 	Title string `json:"Title"`
 	Description string `json:"Description"`
 }
 
-type allEvent []event
+type allEvents []event
 
-var events = allEvent {
+var events = allEvents {
 	{
 		ID: "1",
 		Title: "Introduction to Golang",
 		Description: "Come join us for a chance to learn how golang works and get to eventually try it out",
 	},
+}
+
+// Creating an event
+func createEvent(w http.ResponseWriter, r *http.Request) {
+	var newEvent event
+	reqBody, err := ioutil.ReadAll(r.Body)
+	if err!= nil {
+		fmt.Fprintf(w, "Kindly enter data with the event title and description only in order to update")
+	}
+
+	json.Unmarshal(reqBody, &newEvent)
+	events = append(events, newEvent)
+	w.WriteHeader(http.StatusCreated)
+
+	json.NewEncoder(w).Encode(newEvent)
 }
 
 func homeLink(w http.ResponseWriter, r *http.Request) {
